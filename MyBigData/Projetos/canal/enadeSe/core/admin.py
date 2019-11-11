8a59bea6d7ec445efb2ba6f283bb89da2b1a4d19
Igor Batista <igorslbatista@hotmai.com>
2019-11-11 19:44:51 -0300

    recomeço

--- MyBigData/Projetos/canal/enadeSe/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Questao
-
-admin.site.site_header = 'Página do Administrador - ENADEsE'
-
-# Register your models here.
-@admin.register(Questao)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ['idquestao', 'texto']
